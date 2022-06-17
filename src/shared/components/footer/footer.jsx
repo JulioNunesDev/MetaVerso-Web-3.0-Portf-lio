14ff9de11a2cee937ed4a427025c8e2cc3cce6c3
julionunesdev <julionunes999@gmail.com>
2022-06-17 00:41:35 -0300

    refactor: "footer"

--- src/shared/components/footer/footer.jsx
+++ src/shared/components/footer/footer.jsx
@@ -1,100 +1,74 @@
 import styled from "styled-components";
 import bone from "../../../assets/bone.png";
+import git from "../../../assets/bg/git.png";
+import insta from "../../../assets/bg/insta.png";
+import linke from "../../../assets/bg/linke.png";
 
 const FFooter = styled.footer`
   width: 100%;
   height: 200px;
   background-color: var(--color-bg3);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 
-  .links {
-    width: auto;
-    height: auto;
-
-    img {
-      width: 220px;
-      height: 250px;
-      object-fit: cover;
-    }
-  }
-  .footer-center {
-    overflow: hidden;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
+  .infor-redes {
+    flex-direction: column;
+    width: 50%;
     height: 100%;
-    color: #fafafa;
-    .social-media {
-        display: flex;
-        align-items: center;
-        justify-content: space-around;
-        margin-top: 10px;
-
-        a{
-            display: flex;
-            align-items: center;
-            gap: 5px;
-            color: var(--color-bg2);
-            text-decoration: none;
-            transition: .2s ease-in ;
-        }
-        a:hover{
-            color: #fafafa;
-        }
-    }
-    .social-media a svg {
-      width: 1.1rem;
-      fill: currentColor;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     
+    h2{
+      color: var(--color-bg2);
+      font-size: 18px;
+      margin-bottom: 20px;
+      font-weight: 400;
+      font-family: var(--font-roboto);
     }
 
-    h1 {
-      color: #fafafa;
-    }
   }
+  ul{
+    display: flex;
+    gap: 15px;
+  }
+  a{
+    
+  }
+  img{
+    width: 45px;
+    height: 45px;
+  }
+
+  .links{
+
+    width: 50%;
+    height: 100%;
+    
+  }
+
+  
 `;
 
 const Footer = () => {
   return (
     <FFooter>
-      <div className="footer-center">
-        <div className="links">
-          <img src={bone} alt="" />
-        </div>
-
-        <div className="copy">
-          <h1>COPYRIGHT</h1>
-          &copy; <span>2022 Julio Nunes. All rights reserved.</span>
-          <div className="net">
-            <div class="social-media">
-              <div>
-                <a href="https://twitter.com/julionuneslindo" target='_blank'>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                    <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
-                  </svg>
-                  <span class="tooltip-social">Twitter</span>
-                </a>
-              </div>
-              <div>
-                <a href="https://www.instagram.com/julionunesyt/" target='_blank'>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
-                  </svg>
-                  <span class="tooltip-social">Instagram</span>
-                </a>
-              </div>
-              <div>
-                <a href="https://www.linkedin.com/in/julionunesdev/" target='_blank'>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
-                  </svg>
-                  <span class="tooltip-social">LinkedIn</span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="infor-redes">
+        <h2>Entre em contato conosco</h2>
+        <ul>
+          <a href="https://www.instagram.com/julionunesyt" target="_blank">
+          <img src={insta}  alt="Instagram" />
+          </a>
+          <a href="https://www.linkedin.com/in/julionunesdev/"  target="_blank">
+          <img src={linke}   alt="LinkedIn" />
+          </a>
+          <a href="https://github.com/JulioNunesDev"  target="_blank">
+          <img src={git}  alt="GitHub" />
+          </a>
+        </ul>
       </div>
+      
     </FFooter>
   );
 };
